94b37763d0435b9d246e8e366c39f02364a6b750
Domenico Manuardi s310122 <s310122@studenti.polito.it>
2023-11-19 17:39:22 +0100

    Initial commit

---
+++ src/main/java/it/polito/se2/g04/saml2demo/saml2demo/Saml2DemoApplication.java
@@ -0,0 +1,13 @@
+package it.polito.se2.g04.saml2demo.saml2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saml2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Saml2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/polito/se2/g04/saml2demo/saml2demo/Saml2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polito.se2.g04.saml2demo.saml2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Saml2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
